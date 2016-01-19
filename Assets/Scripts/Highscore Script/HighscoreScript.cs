25ddec0907e73c9f025aaae9f8b4c63271040df2
Kranov <hello2011163@gmail.com>
2016-01-19 09:21:10 +0800

    Added Highscore Script

---
+++ Assets/Scripts/Highscore Script/HighscoreScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HighscoreScript : MonoBehaviour {
+    private int Score;
+
+    public static HighscoreScript currentHighscore;
+
+    Text highscoreLabel;
+
+    void Awake()
+    {
+        currentHighscore = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+        highscoreLabel = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        highscoreLabel.text = "Score: " + Score;
+	}
+
+    public void SetScore(int Score)
+    {
+        this.Score = Score;
+    }
+
+    public int GetScore()
+    {
+        return Score;
+    }
+}
